bcb9252eab8d6b11d4a2ced314b23b856ba17e1f
Panuwatbank <35716517+Panuwatbank@users.noreply.github.com>
2018-04-28 17:23:02 +0700

    Update banksshvpn.sh

--- banksshvpn.sh
+++ banksshvpn.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 tput setaf 7 ; tput setab 4 ; tput bold ; printf '%35s%s%-20s\n' "VPS Manager 2.0.1" ; tput sgr0
 tput setaf 3 ; tput bold ; echo "" ; echo "Este script irá:" ; echo ""
-echo "● Instalar e configurar o proxy squid nas portas 81, 3128, 8080 e 8000" ; echo "  para permitir conexões SSH para este servidor"
-echo "● Configurar o OpenSSH para rodar nas portas 22 e 143"
+echo "● Instalar e configurar o proxy squid nas portas 80, 3128, 8080 e 8799" ; echo "  para permitir conexões SSH para este servidor"
+echo "● Configurar o OpenSSH para rodar nas portas 22 e 443"
 echo "● Instalar um conjunto de scripts como comandos do sistema para o gerenciamento de usuários" ; tput sgr0
 echo ""
 tput setaf 3 ; tput bold ; read -n 1 -s -p "Aperte qualquer tecla para continuar..." ; echo "" ; echo "" ; tput sgr0
